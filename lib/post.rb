69c3b42a530a7b228263448d8cff052f49c9dff8
Ivan <zemchikov0@gmail.com>
2021-11-06 15:23:13 +0300

    version_1

--- lib/post.rb
+++ lib/post.rb
@@ -5,7 +5,21 @@ class Post
   # Конструктор
   def initialize
     @created_at = Time.now # дата создания записи
-    @text = nil # массив строк записи — пока пустой
+    @text = [] # массив строк записи — пока пустой
+  end
+
+  # Набор известных детей класса Запись в виде массива классов
+  def self.post_types
+    [Memo, Task, Link]
+  end
+
+  # Динамическое создание объекта нужного класса из набора возможных детей
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+  # Динамическое создание объекта нужного класса из набора возможных детей
+  def self.create(type_index)
+    return post_types[type_index].new
   end
 
   # Этот метод вызывается в программе, когда нужно
@@ -23,7 +37,7 @@ class Post
 
   # Этот метод записывает текущее состояние объекта в файл
   def save
-    file = File.new(file_path. "w:UTF-8")
+    file = File.new(file_path, "w:UTF-8")
 
     for item in to_strings do
       file.puts(item)
@@ -33,12 +47,13 @@ class Post
   end
 
   def file_path
-    current_path = File.dirname(__FILE__ )
+    current_path = File.dirname(__FILE__)
 
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%s.txt")
 
     "#{current_path}/#{file_name}"
   end
+
 end
 
 # PS: Весь набор методов, объявленных в родительском классе называется интерфейсом класса
--- main.rb
+++ main.rb
@@ -1,1 +1,48 @@
+# Подключаем класс Post и его детей
+require_relative "#{__dir__}/lib/post.rb"
+require_relative "#{__dir__}/lib/memo.rb"
+require_relative "#{__dir__}/lib/link.rb"
+require_relative "#{__dir__}/lib/task.rb"
 
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+# Как обычно, при использовании классов программа выглядит очень лаконично
+puts "Привет, я твой блокнот!"
+
+# Теперь надо спросить у пользователя, что он хочет создать
+puts "Что хотите записать в блокнот?"
+
+# массив возможных видов Записи (поста)
+choices = Post.post_types
+
+choice = -1
+
+until choice >= 0 && choice < choices.size # пока юзер не выбрал правильно
+  # выводим заново массив возможных типов поста
+  choices.each_with_index do |type, index|
+    puts "\t#{index}. #{type}"
+  end
+  choice = gets.chomp.to_i
+end
+
+# выбор сделан, создаем запись с помощью стат. метода класса Post
+entry = Post.create(choice)
+
+# сейчас в переменной entry лежит один из детей класса Post, какой именно,
+# определилось выбором пользователя, переменной choice.
+# Но мы не знаем какой, и обращаемся с entry как с объектом класса Post, этого, оказывается, достаточно.
+
+# Просим пользователя ввести пост (каким бы он ни был)
+entry.read_from_console
+
+# Сохраняем пост в файл
+entry.save
+
+puts "Ваша запись сохранена!"
